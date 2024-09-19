0ccc0ad6d72c83c40080e6630ba30559723e0c59
EliasMiranda02 <9613266784az@gmail.com>
2024-09-19 16:08:50 -0600

    PHP

---
+++ index2.php
@@ -0,0 +1,24 @@
+<html lang="en">
+    <body>
+        <?php
+        echo 'hola PHP';
+        echo '<h1>hola php</h1>';
+        echo '<hr>';
+        echo '<br>';
+        echo '<img src="img/Barra.jpg" alt="">';
+
+
+        echo '<br>';
+        $miNombre='Miranda';
+        echo 'Mi nombre es: '.$miNombre;
+
+        echo '<br>';
+        $miDireccion = '19 poniente sur';
+        echo 'Mi direccion es: ' .$miDireccion;
+
+        echo '<br>';
+        $miNumero = '9614461001';
+        echo 'Mi numero es: ' .$miNumero;
+        ?>
+    </body>
+</html>
